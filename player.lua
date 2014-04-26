1d9785409e1e65e7b47892d53e7b68f247817bb3
richardzhou <various.authors@gmail.com>
2014-04-26 15:01:06 -0700

    Added jumping!

--- player.lua
+++ player.lua
@@ -3,13 +3,14 @@ Player = function (point)
     local p, v = point.copy(), Vector(0, 0)
     local speed, is_jumping = 100, true
     local forces = {
-        key = Vector(0, 0),
-        gravity = Vector(0, 0.1)
+        key        = Vector(0, 0),
+        gravity    = Vector(0, 0.5),
+        resistance = Vector(0.3, 0.3)
     }
 
     local keypressed = function (key)
-        if key == "right" then forces.key.setX(1) end
-        if key == "left"  then forces.key.setX(-1) end
+        if is_jumping then return end
+        if love.keyboard.isDown("up") then forces.key.setY(-10) end
     end
 
     local slowDown = function (dt, get, set)
@@ -25,13 +26,43 @@ Player = function (point)
         return is_jumping
     end
 
+    local drag = function (v)
+        local x, y   = v.getX(), v.getY()
+        local rx, ry = forces.resistance.getX(), forces.resistance.getX()
+
+        if x > 0 then x = math.max(x - rx, 0)
+        else          x = math.min(x + rx, 0) end
+
+        if y > 0 then y = math.max(y - ry, 0)
+        else          y = math.min(y + ry, 0) end
+
+        return Vector(x, y)
+    end
+
+    local setKeyForces = function ()
+        if love.keyboard.isDown("right", "left") then
+            if love.keyboard.isDown("left") then forces.key.setX(-0.5) end
+            if love.keyboard.isDown("right") then forces.key.setX(0.5) end
+        end
+    end
+
     local update = function (dt, map)
+        setKeyForces()
+
         if (forces.key ~= nil) then
             v = v.plus(forces.key)
 
             if isJumping() then
-                v = v.plus(forces.gravity) 
+                v = v.plus(forces.gravity)
             end
+
+            print("===")
+            print(v.getX())
+            print(v.getY())
+            print("===")
+            v = drag(v)
+            print(v.getX())
+            print(v.getY())
         end
 
         p.setY(p.getY() + v.getY() * dt * speed)
