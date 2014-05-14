4964448f5c3575784b01e4e238e30d2bb0ea2bae
richardzhou <various.authors@gmail.com>
2014-05-14 09:35:21 -0700

    Fixed screen scroll to the edge of the world.

--- conf.lua
+++ conf.lua
@@ -1,19 +1,22 @@
 -- globals having to do with the tile library
-global              = {}
-global.limitDrawing = true  -- If true then the drawing range example is shown
-global.benchmark    = false -- If true the map is drawn 20 times instead of 1
-global.useBatch     = false -- If true then the layers are rendered with sprite batches
-global.tx           = 0     -- X translation of the screen
-global.ty           = 0     -- Y translation of the screen
-global.scale        = 3     -- Scale of the screen
-global.tile_size    = 16    -- the pixels in a tile square
-global.tile_height  = 15    -- the tile squares in a window
-global.tile_width   = 16    -- the tile squares in a window
-global.flower_get   = false -- whether a flower was got this tic
-global.flowers      = 0     -- the number of flowers collected so far
-global.double_jump  = false -- EVERYTHING IS GLOBAL NOW...  prorgamming!
-global.walljump     = false -- arbitrary shrine, no gamplay effect
-global.backwards	= false -- HOOK IN HERE ZIGGY, this doesn't actually have gameplay yet
+global               = {}
+global.limitDrawing  = true  -- If true then the drawing range example is shown
+global.benchmark     = false -- If true the map is drawn 20 times instead of 1
+global.useBatch      = false -- If true then the layers are rendered with sprite batches
+global.tx            = 0     -- X translation of the screen
+global.ty            = 0     -- Y translation of the screen
+global.max_tx        = 3072
+global.scale         = 3     -- Scale of the screen
+global.tile_size     = 16    -- the pixels in a tile square
+global.tile_height   = 15    -- the tile squares in a window
+global.tile_width    = 16    -- the tile squares in a window
+global.flower_get    = false -- whether a flower was got this tic
+global.flowers       = 0     -- the number of flowers collected so far
+global.double_jump   = false -- EVERYTHING IS GLOBAL NOW...  prorgamming!
+global.walljump      = false -- arbitrary shrine, no gamplay effect
+global.backwards     = false -- HOOK IN HERE ZIGGY, this doesn't actually have gameplay yet
+global.window_width  = global.tile_size * global.scale * global.tile_height
+global.window_height = global.tile_size * global.scale * global.tile_width
 
 -- debugging stuff
 tile_x        = nil
@@ -28,8 +31,8 @@ teleport      = ""
 
 function love.conf(t)
     -- tile height * scale factor * layer height
-    t.window.height = global.tile_size * global.scale * global.tile_height
-    t.window.width  = global.tile_size * global.scale * global.tile_width
+    t.window.height = global.window_width
+    t.window.width  = global.window_height
     t.window.title = "Super Plumber Bros."
     t.modules.physics = false
 end
